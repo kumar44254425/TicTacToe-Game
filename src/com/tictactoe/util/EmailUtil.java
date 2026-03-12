a72534b1b96e1b1e1ed9e562db3dec3bcc6784b7
gosukulasrija <24ag5a0503@gmail.com>
2026-03-12 23:24:42 +0530

    Fixed EmailUtil authentication

--- src/com/tictactoe/util/EmailUtil.java
+++ src/com/tictactoe/util/EmailUtil.java
@@ -3,32 +3,38 @@ package com.tictactoe.util;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
-import javax.mail.PasswordAuthentication;
 
 public class EmailUtil {
 
     public static void sendOTP(String toEmail, String otp) {
 
         final String fromEmail = "kgosukula@gmail.com";
-        final String password = "xapa xnzj qsms igte";
+        final String password = "aikd dskd ufhy rlmg"; // put new gmail app password
 
         Properties props = new Properties();
 
         props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
         Session session = Session.getInstance(props,
-                new Authenticator() {
+                new javax.mail.Authenticator() {
+
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(fromEmail, password);
                     }
+
                 });
 
         session.setDebug(true);
 
         try {
+
+            System.out.println("Sending OTP to: " + toEmail);
+            System.out.println("OTP = " + otp);
 
             Message message = new MimeMessage(session);
 
@@ -49,5 +55,6 @@ public class EmailUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
